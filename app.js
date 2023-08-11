45f3da3e2b3fc5ef33cc14a914cba2f2dbbd7934
Jesulayomy <jesulayomi081@gmail.com>
2023-08-11 18:36:49 +0000

    changed mogodb connection adress

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use((req, res, next) => {
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/blogDB');
+mongoose.connect('mongodb://127.0.0.1:27017/blogDB');
 
 let errorMessage = '';
 let loginMessage = '';
