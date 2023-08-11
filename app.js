255d7fbc878261eb81c44acf71c03cb5ebe5a28b
micoliser <micoliser@gmail.com>
2023-08-11 18:51:11 +0100

    Changed the app listening port

--- app.js
+++ app.js
@@ -413,6 +413,6 @@ app.post('/compose', (req, res) => {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Server started on port 3000');
+app.listen(3005, function () {
+  console.log('Server started on port 3005');
 });
